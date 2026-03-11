75c759daf4847210fc88c5aafd0671635ae0ea8c
BelieveGamesStudios <afolabifunmilade229@gmail.com>
2026-03-11 17:52:46 +0100

    Update ArLightAdjuster.cs

--- Assets/Scripts/Core/ArLightAdjuster.cs
+++ Assets/Scripts/Core/ArLightAdjuster.cs
@@ -1,24 +1,26 @@
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
-
-[RequireComponent(typeof(ARCameraManager))]
-public class ArLightAdjuster : MonoBehaviour
-{
-    [Header("Components")]
-    private ARCameraManager cameraManager;
-    private Light mainLight;
-    private void Start()
-    {
-        cameraManager = GetComponent<ARCameraManager>();
-        mainLight = FindFirstObjectByType<Light>();
-        cameraManager.frameReceived += CameraManager_frameReceived;
-    }
+namespace Imisi3D
+  {
+  [RequireComponent(typeof(ARCameraManager))]
+  public class ArLightAdjuster : MonoBehaviour
+  {
+      [Header("Components")]
+      private ARCameraManager cameraManager;
+      private Light mainLight;
+      private void Start()
+      {
+          cameraManager = GetComponent<ARCameraManager>();
+          mainLight = FindFirstObjectByType<Light>();
+          cameraManager.frameReceived += CameraManager_frameReceived;
+      }
+    
+      private void CameraManager_frameReceived(ARCameraFrameEventArgs obj)
+      {
+          var lightInfo = obj.lightEstimation.averageBrightness;
+          if(lightInfo.HasValue)
+              mainLight.intensity = lightInfo.Value;
   
-    private void CameraManager_frameReceived(ARCameraFrameEventArgs obj)
-    {
-        var lightInfo = obj.lightEstimation.averageBrightness;
-        if(lightInfo.HasValue)
-            mainLight.intensity = lightInfo.Value;
-
-    }
+      }
+  }
 }
